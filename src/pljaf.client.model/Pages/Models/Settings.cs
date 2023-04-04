97ef5304ff63f1c9a0e6caed0d25f8eda296c67c
filip.vukovinski <filip.vukovinski@net.hr>
2023-04-04 21:58:42 +0200

    added page models

---
+++ src/pljaf.client.model/Pages/Models/Settings.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace pljaf.client.model;
+
+public partial class Settings : ObservableObject
+{
+    [ObservableProperty] private bool? getNotifications;
+}
