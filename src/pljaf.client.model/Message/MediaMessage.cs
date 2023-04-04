eaef8a12173cf73cf8c84cc9b50eddd192b112fc
filip.vukovinski <filip.vukovinski@net.hr>
2023-04-04 09:58:16 +0200

    added x-plat app project

--- src/pljaf.client.model/Message/MediaMessage.cs
+++ src/pljaf.client.model/Message/MediaMessage.cs
@@ -2,5 +2,6 @@
 
 public abstract class MediaMessage : Message, IMediaSource
 {
+    public abstract Task<byte[]> GetMediaDataAsync();
     public abstract MediaReference GetMediaReference();
 }
--- src/pljaf.client.model/Message/TitledMediaMessage.cs
+++ src/pljaf.client.model/Message/TitledMediaMessage.cs
@@ -3,5 +3,6 @@
 public abstract class TitledMediaMessage : Message, IUnicodeBody, IMediaSource
 {
     public abstract string GetTextField();
+    public abstract Task<byte[]> GetMediaDataAsync();
     public abstract MediaReference GetMediaReference();
 }
