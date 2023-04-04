d1479f81b9234b5445448807fe835800bd19df2c
filip.vukovinski <filip.vukovinski@net.hr>
2023-04-04 21:58:19 +0200

    modified client api surface to reflect new server changes

--- src/pljaf.client.model/Mailbox/Conversation.cs
+++ src/pljaf.client.model/Mailbox/Conversation.cs
@@ -6,6 +6,9 @@ public abstract class Conversation
     public required ConversationState State { get; init; }
     public required IEnumerable<Message> Messages { get; init; }
 
+    public string? Name { get; init; }
+    public string? Topic { get; init; }
+
     public abstract Task<Conversation> UpdateConversation();
     public abstract Task<Metadata> SendMessage(OriginalMessage message);
     public abstract Task<Conversation> SendSuccessfull(OriginalMessage message, Metadata metadata);
--- src/pljaf.client.model/User/User.cs
+++ src/pljaf.client.model/User/User.cs
@@ -3,4 +3,5 @@
 public sealed class User
 {
     public required UserId Id { get; init; }
+    public ImageRef? AvatarRef { get; init; }
 }
