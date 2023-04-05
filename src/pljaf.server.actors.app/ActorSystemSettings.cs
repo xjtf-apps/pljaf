24a9f6f91d1a29f90fae15ee658b112eb5194012
filip.vukovinski <filip.vukovinski@net.hr>
2023-04-05 15:10:58 +0200

    added silo worker project

---
+++ src/pljaf.server.actors.app/ActorSystemSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace pljaf.server.actors.app;
+public class ActorSystemSettings
+{
+    public required PortSettings Ports { get; set; }
+    public required ConnectionStringSettings ConnectionStrings { get; set; }
+    public required string ClusterId { get; set; }
+    public required string ServiceId { get; set; }
+
+    public class PortSettings
+    {
+        public required int SiloPorts { get; set; }
+        public required int GatewayPorts { get; set; }
+    }
+
+    public class ConnectionStringSettings
+    {
+        public required string MembershipTable { get; set; }
+        public required string ActorPersistance { get; set; }
+    }
+}
---
+++ src/pljaf.server.actors.app/Program.cs
@@ -0,0 +1,23 @@
+using Orleans.Configuration;
+using pljaf.server.actors.app;
+
+await Host
+    .CreateDefaultBuilder(args)
+    .UseOrleans((context, builder) =>
+    {
+        var hostConfig = context.Configuration;
+        var actorSystemSection = hostConfig.GetRequiredSection("ActorSystem");
+        var actorSystemSettings = actorSystemSection.Get<ActorSystemSettings>()!;
+
+        var siloPorts = actorSystemSettings.Ports.SiloPorts;
+        var gatewayPorts = actorSystemSettings.Ports.GatewayPorts;
+        var connectionStrings = actorSystemSettings.ConnectionStrings;
+
+        builder.ConfigureEndpoints(siloPort: siloPorts, gatewayPort: gatewayPorts);
+        builder.Configure<ClusterOptions>(co => co.ClusterId = actorSystemSettings.ClusterId);
+        builder.Configure<ClusterOptions>(co => co.ServiceId = actorSystemSettings.ServiceId);
+        builder.ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Information).AddConsole());
+        builder.UseAzureStorageClustering(options => options.ConfigureTableServiceClient(connectionStrings.MembershipTable));
+        builder.AddAzureTableGrainStorageAsDefault(options => options.ConfigureTableServiceClient(connectionStrings.ActorPersistance));
+    })
+    .RunConsoleAsync();
