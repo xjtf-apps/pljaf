e1711d636afa62bdee3a7dcf8ab336c323d2ddbc
filip.vukovinski <filip.vukovinski@net.hr>
2023-04-06 00:24:58 +0200

    added refresh token expires field

--- src/pljaf.server.actors.model/Entities/Model/Tokens.cs
+++ src/pljaf.server.actors.model/Entities/Model/Tokens.cs
@@ -5,4 +5,5 @@ public class Tokens
 {
     [Id(0)] public string AccessToken { get; set; }
     [Id(1)] public string RefreshToken { get; set; }
+    [Id(2)] public DateTime RefreshTokenExpires { get; set; }
 }
