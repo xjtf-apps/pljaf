7e77f41e8c31e2baa70e6ce5bd925757b1517624
filip.vukovinski <filip.vukovinski@net.hr>
2023-04-07 16:47:59 +0200

    added default subscription mgmt methods to forwarded observer interface

--- src/pljaf.server.actors.model/Observers/IForwardedClientObserver.cs
+++ src/pljaf.server.actors.model/Observers/IForwardedClientObserver.cs
@@ -10,4 +10,67 @@ public interface IForwardedClientObserver :
     IConvCommunicationChangedObserver,
     IMessageMediaAttachedObserver,
     IMessageAuthoredObserver
-{ }
+{
+    Task SubscribeToGrain(IGrainFactory grainFactory, IGrain grain)
+    {
+        if (grain is IMessageGrain message) return SubscribeToMessageGrain(grainFactory, message);
+        if (grain is IConversationGrain conversation) return SubscribeToConversationGrain(grainFactory, conversation);
+        throw new NotSupportedException(grain.GetType().Name);
+    }
+
+    Task UnsubscribeFromGrain(IGrainFactory grainFactory, IGrain grain)
+    {
+        if (grain is IMessageGrain message) return UnsubscribeFromMessageGrain(grainFactory, message);
+        if (grain is IConversationGrain conversation) return UnsubscribeFromConversationGrain(grainFactory, conversation);
+        throw new NotSupportedException(grain.GetType().Name);
+    }
+
+    Task SubscribeToMessageGrain(IGrainFactory grainFactory, IMessageGrain message)
+    {
+        var _thisMediaAttachedObserver = grainFactory.CreateObjectReference<IMessageMediaAttachedObserver>(this);
+        var _thisMessageAuthoredObserver = grainFactory.CreateObjectReference<IMessageAuthoredObserver>(this);
+        var t1 = message.Subscribe(_thisMediaAttachedObserver);
+        var t2 = message.Subscribe(_thisMessageAuthoredObserver);
+        return Task.WhenAll(t1, t2);
+    }
+
+    Task UnsubscribeFromMessageGrain(IGrainFactory grainFactory, IMessageGrain message)
+    {
+        var _thisMediaAttachedObserver = grainFactory.CreateObjectReference<IMessageMediaAttachedObserver>(this);
+        var _thisMessageAuthoredObserver = grainFactory.CreateObjectReference<IMessageAuthoredObserver>(this);
+        var t1 = message.Unsubscribe(_thisMediaAttachedObserver);
+        var t2 = message.Unsubscribe(_thisMessageAuthoredObserver);
+        return Task.WhenAll(t1, t2);
+    }
+
+    Task SubscribeToConversationGrain(IGrainFactory grainFactory, IConversationGrain conversation)
+    {
+        var _thisNameObserver = grainFactory.CreateObjectReference<IConvNameChangedObserver>(this);
+        var _thisTopicObserver = grainFactory.CreateObjectReference<IConvTopicChangedObserver>(this);
+        var _thisMembersObserver = grainFactory.CreateObjectReference<IConvMembersChangedObserver>(this);
+        var _thisInvitesObserver = grainFactory.CreateObjectReference<IConvInvitesChangedObserver>(this);
+        var _thisCommunicationObserver = grainFactory.CreateObjectReference<IConvCommunicationChangedObserver>(this);
+        var t1 = conversation.Subscribe(_thisNameObserver);
+        var t2 = conversation.Subscribe(_thisTopicObserver);
+        var t3 = conversation.Subscribe(_thisMembersObserver);
+        var t4 = conversation.Subscribe(_thisInvitesObserver);
+        var t5 = conversation.Subscribe(_thisCommunicationObserver);
+        return Task.WhenAll(t1, t2, t3, t4, t5);
+    }
+
+    Task UnsubscribeFromConversationGrain(IGrainFactory grainFactory, IConversationGrain conversation)
+    {
+        var _thisNameObserver = grainFactory.CreateObjectReference<IConvNameChangedObserver>(this);
+        var _thisTopicObserver = grainFactory.CreateObjectReference<IConvTopicChangedObserver>(this);
+        var _thisMembersObserver = grainFactory.CreateObjectReference<IConvMembersChangedObserver>(this);
+        var _thisInvitesObserver = grainFactory.CreateObjectReference<IConvInvitesChangedObserver>(this);
+        var _thisCommunicationObserver = grainFactory.CreateObjectReference<IConvCommunicationChangedObserver>(this);
+        var t1 = conversation.Unsubscribe(_thisNameObserver);
+        var t2 = conversation.Unsubscribe(_thisTopicObserver);
+        var t3 = conversation.Unsubscribe(_thisMembersObserver);
+        var t4 = conversation.Unsubscribe(_thisInvitesObserver);
+        var t5 = conversation.Unsubscribe(_thisCommunicationObserver);
+        return Task.WhenAll(t1, t2, t3, t4, t5);
+    }
+}
+
---
+++ src/pljaf.server.api/Services/ForwardedClientObserverService.cs
@@ -0,0 +1,47 @@
+﻿using pljaf.server.actors.model;
+using pljaf.server.model;
+
+namespace pljaf.server.api;
+
+public class ForwardedClientObserverService : IForwardedClientObserver
+{
+    public Task DownloadAttachedMedia(Media? mediaRef)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task OnMemberInvited(IUserGrain inviter, IUserGrain invited)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task OnMemberJoined(IUserGrain newMember)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task OnMemberLeft(IUserGrain leftMember)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task OnMessagePosted(IMessageGrain message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task OnNameChanged(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task OnTopicChanged(string topic)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ReceiveSentConfirmation(DateTime timestamp)
+    {
+        throw new NotImplementedException();
+    }
+}
