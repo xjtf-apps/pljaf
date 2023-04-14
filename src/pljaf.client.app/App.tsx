e864b16f91300f2e3b1212b392ca2bb9b5fda87b
filip.vukovinski <filip.vukovinski@net.hr>
2023-04-14 17:44:33 +0200

    added react-native ui components

--- src/pljaf.client.app/App.tsx
+++ src/pljaf.client.app/App.tsx
@@ -1,22 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, Text, TextInput, View } from 'react-native';
+import View from 'react-native-ui-lib/view';
+import Text from 'react-native-ui-lib/text';
+import Card from 'react-native-ui-lib/card';
+import Button from 'react-native-ui-lib/button';
+import TextField from 'react-native-ui-lib/textField';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>pljaf</Text>
-      <TextInput style={{
-          height: 40,
-          width: 250,
-          padding: 5,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-        keyboardType="phone-pad"
-        placeholder="Unesi broj svog mobitela"
-      />
-      <Button title="Prijava" key="login" />
-      <StatusBar style="auto" />
+    <View flex paddingH-25 paddingT-250>
+      <Text foreground-primaryColor text20>Dobrodošli</Text>
+      <TextField text50 placeholder="broj telefona" grey10 keyboardType="phone-pad"/>
+      <View marginT-250>
+        <Button text70 white background-secondaryColor label="Prijava"/>
+      </View>
     </View>
   );
 }
