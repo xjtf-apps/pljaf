bfb9bc6ee5a739d35589b22f7597cb8a28e06c06
filip.vukovinski <filip.vukovinski@net.hr>
2023-04-16 00:52:34 +0200

    added flyerhq chat component

--- src/pljaf.client.app/App.tsx
+++ src/pljaf.client.app/App.tsx
@@ -1,28 +1,134 @@
-import { StatusBar } from 'expo-status-bar';
-import View from 'react-native-ui-lib/view';
-import Text from 'react-native-ui-lib/text';
-import Card from 'react-native-ui-lib/card';
-import Button from 'react-native-ui-lib/button';
-import TextField from 'react-native-ui-lib/textField';
-import { StyleSheet } from 'react-native';
+import { useActionSheet } from '@expo/react-native-action-sheet'
+import { Chat, MessageType } from '@flyerhq/react-native-chat-ui'
+import { PreviewData } from '@flyerhq/react-native-link-preview'
+import React, { useState } from 'react'
+import DocumentPicker from 'react-native-document-picker'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { launchImageLibrary } from 'react-native-image-picker'
 
-export default function App() {
-  return (
-    <View flex paddingH-25 paddingT-250>
-      <Text foreground-primaryColor text20>Dobrodošli</Text>
-      <TextField text50 placeholder="broj telefona" grey10 keyboardType="phone-pad"/>
-      <View marginT-250>
-        <Button text70 white background-secondaryColor label="Prijava"/>
-      </View>
-    </View>
-  );
+// For the testing purposes, you should probably use https://github.com/uuidjs/uuid
+const uuidv4 = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = Math.floor(Math.random() * 16)
+    const v = c === 'x' ? r : (r % 4) + 8
+    return v.toString(16)
+  })
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  const { showActionSheetWithOptions } = useActionSheet()
+  const [messages, setMessages] = useState<MessageType.Any[]>([])
+  const user = { id: '06c33e8b-e835-4736-80f4-63f44b66666c' }
+
+  const addMessage = (message: MessageType.Any) => {
+    setMessages([message, ...messages])
+  }
+
+  const handleAttachmentPress = () => {
+    showActionSheetWithOptions(
+      {
+        options: ['Photo', 'File', 'Cancel'],
+        cancelButtonIndex: 2,
+      },
+      (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            handleImageSelection()
+            break
+          case 1:
+            handleFileSelection()
+            break
+        }
+      }
+    )
+  }
+
+  const handleFileSelection = async () => {
+    try {
+      const response = await DocumentPicker.pickSingle({
+        type: [DocumentPicker.types.allFiles],
+      })
+      const fileMessage: MessageType.File = {
+        author: user,
+        createdAt: Date.now(),
+        id: uuidv4(),
+        mimeType: response.type ?? undefined,
+        name: response.name,
+        size: response.size ?? 0,
+        type: 'file',
+        uri: response.uri,
+      }
+      addMessage(fileMessage)
+    } catch {}
+  }
+
+  const handleImageSelection = () => {
+    launchImageLibrary(
+      {
+        includeBase64: true,
+        maxWidth: 1440,
+        mediaType: 'photo',
+        quality: 0.7,
+      },
+      ({ assets }) => {
+        const response = assets?.[0]
+
+        if (response?.base64) {
+          const imageMessage: MessageType.Image = {
+            author: user,
+            createdAt: Date.now(),
+            height: response.height,
+            id: uuidv4(),
+            name: response.fileName ?? response.uri?.split('/').pop() ?? '🖼',
+            size: response.fileSize ?? 0,
+            type: 'image',
+            uri: `data:image/*;base64,${response.base64}`,
+            width: response.width,
+          }
+          addMessage(imageMessage)
+        }
+      }
+    )
+  }
+
+  const handlePreviewDataFetched = ({
+    message,
+    previewData,
+  }: {
+    message: MessageType.Text
+    previewData: PreviewData
+  }) => {
+    setMessages(
+      messages.map<MessageType.Any>((m) =>
+        m.id === message.id ? { ...m, previewData } : m
+      )
+    )
+  }
+
+  const handleSendPress = (message: MessageType.PartialText) => {
+    const textMessage: MessageType.Text = {
+      author: user,
+      createdAt: Date.now(),
+      id: uuidv4(),
+      text: message.text,
+      type: 'text',
+    }
+    addMessage(textMessage)
+  }
+
+  return (
+    // Remove this provider if already registered elsewhere
+    // or you have React Navigation set up
+    <SafeAreaProvider>
+      <Chat
+        messages={messages}
+        onAttachmentPress={handleAttachmentPress}
+        onPreviewDataFetched={handlePreviewDataFetched}
+        onSendPress={handleSendPress}
+        user={user}
+      />
+    </SafeAreaProvider>
+  )
+}
+
+export default App
